56b27ac40be9f57082006d5b948d07172038de92
prudvi <94795259+prudvi14284@users.noreply.github.com>
2022-12-01 18:37:44 +0530

    Update Book.cs

--- BookListRazor/Model/Book.cs
+++ BookListRazor/Model/Book.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-ghgjhhgjgh
+
 namespace BookListRazor.Model
 {
     public class Book
